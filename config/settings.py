3324455b432f847b328fed083e5b415b16603d9f
taeho0888 <taeho0888@naver.com>
2023-07-13 02:22:31 +0900

    ✨ 유저 모델 구현 중 (#11)

--- config/settings.py
+++ config/settings.py
@@ -116,6 +116,8 @@ DATABASES = {
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
 
+AUTH_USER_MODEL = 'users.User'
+
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
--- users/models.py
+++ users/models.py
@@ -1,3 +1,47 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 
-# Create your models here.
+
+class UserManager(BaseUserManager):
+    def create_user(self, backjoon_id, github_id, password=None, **extra_fields):
+        if not backjoon_id:
+            raise ValueError('The given backjoon_id must be set')
+        if not github_id:
+            raise ValueError('The given github_id must be set')
+
+        user = self.model(
+            backjoon_id=backjoon_id,
+            github_id=github_id,
+            **extra_fields
+        )
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, backjoon_id, github_id, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+
+        return self.create_user(backjoon_id, github_id, password, **extra_fields)
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    backjoon_id = models.CharField(max_length=255, unique=True)
+    github_id = models.CharField(max_length=255, unique=True)
+    profile_image = models.URLField(blank=True, null=True, default="")
+    company = models.CharField(max_length=255, blank=True, null=True)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'backjoon_id'
+    REQUIRED_FIELDS = ['github_id']
+
+    def __str__(self):
+        return self.backjoon_id
