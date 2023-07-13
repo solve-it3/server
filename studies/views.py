d0146de8758cd0c6db52490068afd04c9ff6bc78
sunghyun1356 <sunghyun1356@naver.com>
2023-07-13 20:49:40 +0900

    :memo::sparkle: serialiers.py, urls.py, forms.py 파일 추가 + StudyNameDuplicatedView serial 추가(#12)

--- studies/views.py
+++ studies/views.py
@@ -11,10 +11,14 @@ from rest_framework.response import Response
 class StudyNameDuplicatedView(generics.RetrieveAPIView):
     queryset = Study.objects.all()
     serializer_class = StudyNameDuplicatedSerializer
-
+    # 보는 것만 해야하니까 RetrieveAPIView만 만들어준다
+    # retrieve를 오버라이딩해준다
     def retrieve(self, request, *args, **kwargs):
+        # instance는 딕셔너리 json파일 형태로 하고
         instance = {}
         try:
+            #Study의 객체들에서 name이 data.get한것의 name을 가져온다
+            #이 객체가 실패하면 False, 유일하면 True를 가져온다
             Study.objects.get(name=request.data.get('name'))
             instance['is_unique'] = False
 
