5a34c54697d09cd64a56816d1a183db103edb083
taeho0888 <taeho0888@naver.com>
2023-07-19 15:38:47 +0900

    :fire: 불필요한 import 삭제 (#11)

--- users/views.py
+++ users/views.py
@@ -4,7 +4,7 @@ from django.conf import settings
 from django.shortcuts import redirect
 
 from rest_framework import status
-from rest_framework.generics import ListAPIView, RetrieveAPIView
+from rest_framework.generics import RetrieveAPIView
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
