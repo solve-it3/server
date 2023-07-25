6048238e32222d0bdefefcdd6b1f070d0b869d6d
taeho0888 <taeho0888@naver.com>
2023-07-25 17:30:44 +0900

    :recycle: 코드 리팩토링

--- studies/urls.py
+++ studies/urls.py
@@ -1,20 +1,23 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import *
+from .views import (
+    DateRecordAPIView,
+    ProblemCreateDestroyAPIView,
+    StudyModelViewSet,
+    StudyNameDuplicatedView,
+    UserStudyHomepageAPIView,
+    WeekRetrieveAPIView,
+)
 
 router = DefaultRouter()
-
 router.register('', StudyModelViewSet)
-
-# register를 study-create로 StudyModelViewSet으로 해줬습니다
 
 urlpatterns = [
     path('', include(router.urls)),
     path('study-name-is-unique/', StudyNameDuplicatedView.as_view()),
-    path('study-date-record/<str:study_name>/<str:solved_at>/',
-        DateRecordAPIView.as_view()),
+    path('study-date-record/<str:study_name>/<str:solved_at>/', DateRecordAPIView.as_view()),
     path('home/<str:study_name>/', UserStudyHomepageAPIView.as_view()),
     path('<str:study_name>/week<int:week_num>/', WeekRetrieveAPIView.as_view()),
-    path('<str:study_name>/week<int:week_num>/problem/',ProblemCreateAPIView.as_view()),
-    path('<str:study_name>/week<int:week_num>/<int:problem_num>/delete',ProblemDestroyAPIView.as_view())
+    path('<str:study_name>/week<int:week_num>/<int:problem_num>/',ProblemCreateDestroyAPIView.as_view()),
+
 ]
